d96ed2f6bf67da9e7cd3f8cef7516008433bbbea
Guillem Gari <ggari@robotnik.es>
2023-11-30 13:48:46 +0100

    removed old files

--- scripts/destroy-cluster.sh
+++
@@ -1,7 +0,0 @@
-sudo rm -f \
-  /etc/cni/net.d/10-calico.conflist \
-  /etc/cni/net.d/calico-kubeconfig \
-  /etc/cni/net.d/10-flannel.conflist
-sudo kubeadm reset --force
-sudo ifconfig cni0 dow
-sudo ip link delete cni0
